2b2b76dc7362478aa3fa0d12facd3ef3c2e8ac44
alanma1993 <mazhen@cmhi.chinamobile.com>
2019-08-08 22:46:15 +0800

    SpringBoot Deepth chapter3 update1 19/8/8

--- SpringBoot/SpringDeepth/chapter3/src/main/java/com/springboot/chapter3/pojo/BusinessPerson.java
+++ SpringBoot/SpringDeepth/chapter3/src/main/java/com/springboot/chapter3/pojo/BusinessPerson.java
@@ -3,6 +3,7 @@ package com.springboot.chapter3.pojo;
 import com.springboot.chapter3.pojo.definition.Animal;
 import com.springboot.chapter3.pojo.definition.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,8 +20,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class BusinessPerson implements Person {
 
+    /**
+     * 关键字 @Primary、@Querlifier用于消除@Autowired的歧义性
+     * @Autowired 自动装载，会根据类型（by type）/名称(by var name)自动获取对应的Bean。
+     * 如果该类型是一个接口，又有多个类实现了该接口，则根据@Autowired根据接口类型获取Bean时，有多个候选项，
+     * 存在歧义性，引发异常。 简单的解决方法是通过变量名区分，但是这样代码失去一定灵活性。
+     * @Primary 表示优先级，与@Component同时使用，有@Primary注解的Bean在遇到@Autowired歧义时，被优先选取。
+     * 但是@Primary可同时用于多个Bean，又会引发歧义性，未根本解决歧义的问题
+     * @Querlifier 关键字的value通过一个字符串定义要获取的Bean名称，与@Autowired同时使用。
+     * 这样，@Autowired获取Bean的顺序是：
+     * 先看前后行存在@@Querlifier关键字没有，如果有，@Querlifier内的value值为最优先级，解决存在多个@Primary候选Bean
+     * 再看@Primary，它定义的Bean优先被获取
+     * 最后再看可候选的Bean，如果还存在多个，则产生歧义异常
+     */
     @Autowired
+    @Qualifier(value="dog")
     private Animal animal = null;
+
+    /**
+     * 带有参数的构造方法类的装配
+     * 1. 先删除存在@Autowired注解的方法或属性的@Autowired注解
+     * 2. 在参数上加上@Autowired @Qualifier注解，使得参数能够注入进来（能注入到IoC容器被IoC容器使用生成Bean）。
+     * @Qualifier 注解为了防止歧义，存在歧义的风险时才有必要加
+     *
+     */
+    /**
+    public BusinessPerson(@Autowired @Qualifier("dog") Animal animal) {
+        this.animal = animal;
+    }
+     */
 
     @Override
     public void service() {
@@ -31,4 +59,11 @@ public class BusinessPerson implements Person {
     public void setAnimal(Animal animal) {
         this.animal = animal;
     }
+
+    /**
+     * Bean的生命周期（Spring IoC初始化和销毁Bean的过程）
+     * 0. 先捋顺Bean如何装配到IoC容器中
+     * 1. 资源定位
+     * 通过配置：@ComponentScan定义的扫描路径和
+     */
 }
